61d960784f36f41b878498ca07f47f4f6c60dc6a
Olenz1 <54223329+Olenz1@users.noreply.github.com>
2021-11-14 12:19:38 +0800

    Update sieve2.c

--- sieve2.c
+++ sieve2.c
@@ -1,11 +1,3 @@
-/*
- *   Sieve of Eratosthenes
- *
- *   Programmed by Michael J. Quinn
- *
- *   Last modification: 7 September 2001
- */
-
 #include "mpi.h"
 #include <math.h>
 #include <stdio.h>
@@ -17,7 +9,6 @@ int main (int argc, char *argv[])
    unsigned long int    count;        /* Local prime count */
    double elapsed_time; /* Parallel execution time */
    unsigned long int    first;        /* Index of first multiple */
-   int   local_first;
    unsigned long int    global_count = 0; /* Global prime count */
    unsigned long long int    high_value;   /* Highest value on this proc */
    unsigned long int    i;
@@ -25,15 +16,13 @@ int main (int argc, char *argv[])
    unsigned long int    index;        /* Index of current prime */
    unsigned long long int    low_value;    /* Lowest value on this proc */
    char  *marked;       /* Portion of 2,...,'n' */
-   char  *local_prime_marked;
    unsigned long long int    n;            /* Sieving from 2, ..., 'n' */
    int    p;            /* Number of processes */
    unsigned long int    proc0_size;   /* Size of proc 0's subarray */
-   unsigned long int    prime;
-   unsigned long int  local_prime;        /* Current prime */
+   unsigned long int    prime;        /* Current prime */
    unsigned long int    size;         /* Elements in 'marked' */
-   unsigned long int  local_prime_size;
-
+   
+   //unsigned long int    evencount; /* Number of evencount */
 
    MPI_Init (&argc, &argv);
 
@@ -51,29 +40,52 @@ int main (int argc, char *argv[])
    }
 
    n = atoll(argv[1]);
-
-   /* Figure out this process's share of the array, as
-      well as the integers represented by the first and
-      last array elements */
-
-   /* Add you code here  */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
    /* Stop the timer */
 
+   /* Add you code here  */
+   
+   low_value = 2 + id * (n - 1) / p;
+   high_value = 1 + (id + 1) * (n - 1) / p;
+   size = high_value - low_value + 1;
+
+   proc0_size = (n - 1) / p;
+
+   if ((2 + proc0_size) < (int) sqrt((double) n)) {
+      if (!id) printf("Too many processes\n");
+      MPI_Finalize();
+      exit(1);
+   }
+
+   marked = (char *) malloc(size);
+
+   if (marked == NULL) {
+      printf("Cannot allocate enough memory\n");
+      MPI_Finalize();
+      exit(1);
+   }
+
+   for (i = 0; i < n; i++) { if (i != 0 && i % 2 == 0) marked[i] = 1; else marked[i] = 0; }   if (!id) index = 0;
+   prime = 2;
+   do {
+      if (prime * prime > low_value)
+         first = prime * prime - low_value;
+      else {
+         if (!(low_value % prime)) first = 0;
+         else first = prime - (low_value % prime);
+      }
+      for (i = first; i < n - 1; i += prime) if (i % 2 != 0) marked[i] = 1;
+      if (!id) {
+            while (marked[++index]);
+            prime = index + 2;
+      }
+      if (p > 1) MPI_Bcast(&prime, 1, MPI_INT, 0, MPI_COMM_WORLD);
+   } while (prime <= sqrt(n));
+   count = 0;
+   for (i = 0; i < size; i++)
+      if (!marked[i]) count++;
+   if (p > 1)
+      MPI_Reduce(&count, &global_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+   
    elapsed_time += MPI_Wtime();
 
 
